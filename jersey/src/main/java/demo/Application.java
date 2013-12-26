e373f14d0e8f669541df40e47a9b58152c0e7fd6
Josh Long <josh@joshlong.com>
2013-12-25 21:26:00 -0800

    updated Jersey example.

--- jersey/src/main/java/demo/Application.java
+++ jersey/src/main/java/demo/Application.java
@@ -1,8 +1,8 @@
 package demo;
 
-import com.sun.jersey.api.core.PackagesResourceConfig;
-import com.sun.jersey.api.core.ResourceConfig;
-import com.sun.jersey.spi.container.servlet.ServletContainer;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,39 +10,35 @@ import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.util.ClassUtils;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
-    @Bean
-    public ServletRegistrationBean jerseyServlet() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/*");
-         registration.addInitParameter(ServletContainer.APPLICATION_CONFIG_CLASS, JaxRsApplication.class.getName());
-        return registration;
-    }
+	@Bean
+	public ServletRegistrationBean jerseyServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new ServletContainer(), "/*");
+		registration.addInitParameter(ServletContainer.APPLICATION_CONFIG_CLASS, JaxRsApplication.class.getName());
+		return registration;
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
-
-    public static class JaxRsApplication extends javax.ws.rs.core.Application {
-        @Override
-        public Set<Class<?>> getClasses() {
-            Set<Class<?>> s = new HashSet<Class<?>>();
-            s.add(Endpoint.class);
-            return s;
-        }
-    }
+	public static class JaxRsApplication extends javax.ws.rs.core.Application {
+		@Override
+		public Set<Class<?>> getClasses() {
+			Set<Class<?>> s = new HashSet<Class<?>>();
+			s.add(Endpoint.class);
+			return s;
+		}
+	}
 }
-
