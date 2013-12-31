c7584a4c04b3e26a725ff8c32c9cc610f9dd6651
Josh Long <josh@joshlong.com>
2013-12-31 03:46:47 -0800

    revised security layer.

--- x-auth-security/src/main/java/example/Application.java
+++ x-auth-security/src/main/java/example/Application.java
@@ -1,67 +1,41 @@
 package example;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.GenericFilterBean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -119,15 +93,9 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-		authManagerBuilder.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin").password("password").roles("USER", "ADMIN");
+		authManagerBuilder.userDetailsService(new CustomUserDetailsService());
 	}
 
-	@Override
-	protected UserDetailsService userDetailsService() {
-		return new CustomUserDetailsService();
-	}
-
-	// Expose the UserDetailsService as a Bean
 	@Bean
 	@Override
 	public UserDetailsService userDetailsServiceBean() throws Exception {
@@ -154,17 +122,17 @@ class CustomFilter extends GenericFilterBean {
 		try {
 			HttpServletRequest httpServletRequest = (HttpServletRequest) arg0;
 			String authToken = httpServletRequest.getHeader("x-test");
-			
+
 			if (StringUtils.hasText(authToken) && authToken.equalsIgnoreCase("nou")) {
-				
+
 				UserDetails userDetails = detailsService.loadUserByUsername("user");
 
 				UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
 				SecurityContextHolder.getContext().setAuthentication(token);
 			}
-			
+
 			filterChain.doFilter(arg0, arg1);
-			
+
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
@@ -173,59 +141,61 @@ class CustomFilter extends GenericFilterBean {
 }
 
 class CustomUserDetailsService implements UserDetailsService {
-	private UserDetails details = new CustomUserDetails();
+
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_USER = "ROLE_USER";
+
+	private UserDetails details = new UserDetails() {
+		private boolean enabled = true;
+
+		public String toString() {
+			return "{enabled:" + enabled + ", username:'" + getUsername() + "', password:'" + getPassword() + "'}";
+		}
+
+		@Override
+		public boolean isEnabled() {
+			return this.enabled;
+		}
+
+		@Override
+		public boolean isCredentialsNonExpired() {
+			return this.enabled;
+		}
+
+		@Override
+		public boolean isAccountNonLocked() {
+			return this.enabled;
+		}
+
+		@Override
+		public boolean isAccountNonExpired() {
+			return this.enabled;
+		}
+
+		@Override
+		public String getUsername() {
+			return "mikey";
+		}
+
+		@Override
+		public String getPassword() {
+			return "bear";
+		}
+
+		@Override
+		public Collection<? extends GrantedAuthority> getAuthorities() {
+			Collection<String> roles = Arrays.<String> asList(ROLE_USER, ROLE_ADMIN);
+			Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			for (String r : roles) {
+				authorities.add(new SimpleGrantedAuthority(r));
+			}
+			return authorities;
+		}
+
+	};
 
 	@Override
 	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
 		return this.details;
-	}
-}
-
-@SuppressWarnings("serial")
-class CustomUserDetails implements UserDetails {
-
-	public CustomUserDetails() {
-	}
-
-	private boolean enabled = true;
-
-	@Override
-	public boolean isEnabled() {
-		return this.enabled;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return this.enabled;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return this.enabled;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return this.enabled;
-	}
-
-	@Override
-	public String getUsername() {
-		return "mikey";
-	}
-
-	@Override
-	public String getPassword() {
-		return "bear";
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Collection<String> roles = Arrays.<String> asList("user", "admin");
-		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-		for (String r : roles) {
-			authorities.add(new SimpleGrantedAuthority(r));
-		}
-		return authorities;
 	}
 }
