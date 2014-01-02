9a50f286b011c51650581b710f5087971dee8f14
Josh Long <josh@joshlong.com>
2014-01-01 20:41:34 -0800

    end-to-end x-auth implementation.

--- x-auth-security/src/main/java/example/Application.java
+++ x-auth-security/src/main/java/example/Application.java
@@ -1,9 +1,14 @@
 package example;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,14 +16,13 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -27,23 +31,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.codec.Hex;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.GenericFilterBean;
- 
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -54,52 +58,111 @@ public class Application {
 	}
 }
 
-class Greeting {
-
-	private String message;
-
-	public Greeting() {
-	}
-
-	public Greeting(String msg) {
-		this.message = msg;
-	}
-
-	public String getMessage() {
-		return this.message;
-	}
-
-	public void setMessage(String m) {
-		this.message = m;
-	}
-}
-
-@RestController
-class GreetingController {
-
-	public static final String GREETING_NAME = "greeting";
-
-	@RequestMapping("/" + GREETING_NAME + "/{name}")
-	Greeting greet(@PathVariable String name) {
-		return new Greeting(name);
-	}
-
-}
-
 /**
- * This controller generates the token that must be 
- * present in subsequent REST invocations. 
+ * This controller generates the token that must be present in subsequent REST
+ * invocations.
  */
 @RestController
 class UserTransferController {
-	
-	@RequestMapping(value = "/auth", method = POST)
-	ResponseEntity<UserTransfer> auth(@RequestParam String username, @RequestParam String password) {
-		return null;
+
+	private final TokenUtils tokenUtils;
+	private final AuthenticationManager authenticationManager;
+	private final UserDetailsService userDetailsService;
+
+	@Autowired
+	public UserTransferController(AuthenticationManager am, UserDetailsService userDetailsService, TokenUtils tokenUtils) {
+		this.tokenUtils = tokenUtils;
+		this.authenticationManager = am;
+		this.userDetailsService = userDetailsService;
+	}
+
+	@RequestMapping(value = "/auth")
+	public UserTransfer authorize(@RequestParam String username, @RequestParam String password) {
+
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+		Authentication authentication = this.authenticationManager.authenticate(token);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		UserDetails details = this.userDetailsService.loadUserByUsername(username);
+
+		Map<String, Boolean> roles = new HashMap<String, Boolean>();
+		for (GrantedAuthority authority : details.getAuthorities())
+			roles.put(authority.toString(), Boolean.TRUE);
+
+		return new UserTransfer(details.getUsername(), roles, tokenUtils.createToken(details));
 	}
 
 	public static class UserTransfer {
 
+		private final String name;
+		private final Map<String, Boolean> roles;
+		private final String token;
+
+		public UserTransfer(String userName, Map<String, Boolean> roles, String token) {
+
+			Map<String, Boolean> mapOfRoles = new ConcurrentHashMap<String, Boolean>();
+			for (String k : roles.keySet())
+				mapOfRoles.put(k, roles.get(k));
+
+			this.roles = mapOfRoles;
+			this.token = token;
+			this.name = userName;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public Map<String, Boolean> getRoles() {
+			return this.roles;
+		}
+
+		public String getToken() {
+			return this.token;
+		}
+	}
+}
+
+@Component
+class TokenUtils {
+
+	public static final String MAGIC_KEY = "obfuscate";
+
+	public String createToken(UserDetails userDetails) {
+		long expires = System.currentTimeMillis() + 1000L * 60 * 60;
+		return userDetails.getUsername() + ":" + expires + ":" + computeSignature(userDetails, expires);
+	}
+
+	public String computeSignature(UserDetails userDetails, long expires) {
+		StringBuilder signatureBuilder = new StringBuilder();
+		signatureBuilder.append(userDetails.getUsername()).append(":");
+		signatureBuilder.append(expires).append(":");
+		signatureBuilder.append(userDetails.getPassword()).append(":");
+		signatureBuilder.append(TokenUtils.MAGIC_KEY);
+
+		MessageDigest digest;
+		try {
+			digest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException("No MD5 algorithm available!");
+		}
+		return new String(Hex.encode(digest.digest(signatureBuilder.toString().getBytes())));
+	}
+
+	public String getUserNameFromToken(String authToken) {
+		if (null == authToken) {
+			return null;
+		}
+		String[] parts = authToken.split(":");
+		return parts[0];
+	}
+
+	public boolean validateToken(String authToken, UserDetails userDetails) {
+		String[] parts = authToken.split(":");
+		long expires = Long.parseLong(parts[1]);
+		String signature = parts[2];
+		String signatureToMatch = computeSignature(userDetails, expires);
+		return expires >= System.currentTimeMillis() && signature.equals(signatureToMatch);
 	}
 }
 
@@ -109,12 +172,17 @@ class UserTransferController {
 @Order
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private TokenUtils tokenUtils;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+
+		CustomFilter customFilter = new CustomFilter(tokenUtils, userDetailsServiceBean());
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.addFilterBefore(new CustomFilter(userDetailsServiceBean()), UsernamePasswordAuthenticationFilter.class);
-		http.authorizeRequests().antMatchers("/" + GreetingController.GREETING_NAME + "/**").hasRole( 
-				CustomUserDetailsService.ROLE_USER);
+		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests().antMatchers("/" + GreetingController.GREETING_NAME + "/**").hasRole(CustomUserDetailsService.ROLE_USER);
 	}
 
 	@Override
@@ -138,27 +206,31 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 class CustomFilter extends GenericFilterBean {
 
 	private UserDetailsService detailsService;
+	private TokenUtils tokenUtils;
+	private String xAuthTokenHeaderName = "x-auth-token";
 
-	public CustomFilter(UserDetailsService userDetailsService) {
+	public CustomFilter(TokenUtils tokenUtils, UserDetailsService userDetailsService) {
 		this.detailsService = userDetailsService;
+		this.tokenUtils = tokenUtils;
 	}
 
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain filterChain) throws IOException, ServletException {
 		try {
 			HttpServletRequest httpServletRequest = (HttpServletRequest) arg0;
-			String authToken = httpServletRequest.getHeader("x-test");
+			String authToken = httpServletRequest.getHeader(this.xAuthTokenHeaderName);
 
-			if (StringUtils.hasText(authToken) && authToken.equalsIgnoreCase("nou")) {
+			if (StringUtils.hasText(authToken)) {
+				String username = this.tokenUtils.getUserNameFromToken(authToken);
 
-				UserDetails userDetails = detailsService.loadUserByUsername("user");
+				UserDetails details = this.detailsService.loadUserByUsername(username);
 
-				UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
-				SecurityContextHolder.getContext().setAuthentication(token);
+				if (this.tokenUtils.validateToken(authToken, details)) {
+					UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(details, details.getPassword(), details.getAuthorities());
+					SecurityContextHolder.getContext().setAuthentication(token);
+				}
 			}
-
 			filterChain.doFilter(arg0, arg1);
-
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
@@ -167,7 +239,7 @@ class CustomFilter extends GenericFilterBean {
 }
 
 class CustomUserDetailsService implements UserDetailsService {
-	
+
 	public static final String ROLE_ADMIN = "ADMIN";
 	public static final String ROLE_USER = "USER";
 
@@ -176,7 +248,7 @@ class CustomUserDetailsService implements UserDetailsService {
 		private boolean enabled = true;
 
 		public String toString() {
-			return "{enabled:" + enabled + ", username:'" + getUsername() + "', password:'" + getPassword() + "'}";
+			return "{enabled:" + isEnabled() + ", username:'" + getUsername() + "', password:'" + getPassword() + "'}";
 		}
 
 		@Override
@@ -209,9 +281,13 @@ class CustomUserDetailsService implements UserDetailsService {
 			return "bear";
 		}
 
+		private String role(String i) {
+			return "ROLE_" + i;
+		}
+
 		@Override
 		public Collection<? extends GrantedAuthority> getAuthorities() {
-			Collection<String> roles = Arrays.<String> asList(ROLE_USER, ROLE_ADMIN);
+			Collection<String> roles = Arrays.<String> asList(role(ROLE_USER), role(ROLE_ADMIN));
 			Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 			for (String r : roles) {
 				authorities.add(new SimpleGrantedAuthority(r));
@@ -222,7 +298,42 @@ class CustomUserDetailsService implements UserDetailsService {
 	};
 
 	@Override
-	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-		return this.details;
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if (username.equalsIgnoreCase(this.details.getUsername())) {
+			return this.details;
+		}
+		return null;
+	}
+}
+
+// greeting REST service
+class Greeting {
+
+	private String message;
+
+	public Greeting() {
+	}
+
+	public Greeting(String msg) {
+		this.message = msg;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String m) {
+		this.message = m;
+	}
+}
+
+@RestController
+class GreetingController {
+
+	public static final String GREETING_NAME = "greeting";
+
+	@RequestMapping("/" + GREETING_NAME + "/{name}")
+	public Greeting greet(@PathVariable String name) {
+		return new Greeting(name);
 	}
 }
