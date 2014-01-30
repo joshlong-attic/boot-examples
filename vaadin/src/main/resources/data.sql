8fa7fced474138ade7e3a42968fb351b04f814eb
Josh Long <josh@joshlong.com>
2014-01-30 05:25:31 -0800

    updated Vaadin demo

--- vaadin/src/main/resources/data.sql
+++ vaadin/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into customer(first_name,last_name) values('Joonas Lehtinen','Lehtinen');
+insert into customer(first_name,last_name) values('Joonas','Lehtinen');
 insert into customer(first_name,last_name) values('Petter','Holmström');
 insert into customer(first_name,last_name) values('Josh','Long');
